e95f1b3a6f21eb511dd3b13ed834ed5532ed83d0
chistev <stephenowabie@gmail.com>
2025-07-03 07:10:36 +0100

    ajax pagination with HTMX

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,21 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.http import HttpRequest
 from .models import Post
 
-def blog_index(request):
+def blog_index(request: HttpRequest):
     post_list = Post.objects.all()
     paginator = Paginator(post_list, 4)
-
     page_number = request.GET.get('page')
     posts = paginator.get_page(page_number)
 
-    return render(request, 'blog/index.html', {'posts': posts})
+    if request.headers.get('HX-Request'):
+        # Return partial if HTMX request
+        return render(request, 'blog/partials/post_list.html', {'posts': posts})
+    else:
+        # Full page for initial load
+        return render(request, 'blog/index.html', {'posts': posts})
+
 
 def categories(request):
     return render(request, 'blog/categories.html')
